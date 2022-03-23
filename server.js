cf9b0cc50cc684e02057b0157fdf5edf00d18405
natanael <nathan0002@outlook.es>
2022-03-22 22:05:22 -0400

    Fixed a problem with the port

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
-const PORT = require(process.env.PORT);
+const PORT = process.env.PORT;
 
 morgan("tiny");
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Listening port ${PORT}`);
