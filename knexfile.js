18ca7f82f9046bd174bfa2db0186dba51c97bde3
natanael <nathan0002@outlook.es>
2022-04-12 23:09:04 -0400

    change the production letter to lowercase

--- knexfile.js
+++ knexfile.js
@@ -6,13 +6,13 @@ module.exports = {
     connection: {
       host: "127.0.0.1",
       user: "root",
-      password: "N03252000$",
+      password: process.env.MYSQL_PASSWORD,
       database: "shopitall_db",
       charset: "utf8",
     },
   },
 
-  Production: {
+  production: {
     client: "mysql",
     connection: process.env.JAWSDB_URL,
   },
